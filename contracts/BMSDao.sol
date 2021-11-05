2d84fd57b53c4fadb27232e0987743773ee9a63f
Benevolent Moon Gaming and Finance <91615238+Benevolent-Moon@users.noreply.github.com>
2021-11-05 07:52:22 -0400

    Update BMSDao.sol
    To Start there will be tiers of release to the DAO to assure that roadmapped components can reach fruition, and find a stable presence.   
    
    Initial elements that can be governed by early DAO governance include,but are not limited to:
    Proposals and Voting on which tokens to offer in games.
    Proposals and Voting on algorithmic strategies for determining the Moon Price in the Benevolent Moon Squares Game.
    Proposals and Voting on the number of tokens to be offered to play. 
    Proposals and Voting on Benevolent Organizations to offer users. 
    Proposals and Voting on which tokens to accept for entry fees.
    
    
    Secondary areas of governance upon the launch and stability of the NFT lottery and galleries include but are not limited to: 
    Proposals and Voting on method and process of executing the NFT Lottery
    Proposals and Voting on determining  BMS fees and use for NFT trading
    Proposals and Voting on Benevolent Organizations to receive percentages of trading fees . 
    
    Tertiary areas of governance upon launch of liquidity, lending and borrowing instruments (Defi). 
    Proposals and Voting on which initial pairs to offer (along with BMS)
    Proposals and Voting on which Benevolent Organizations will receive percentage of trading fees. 
    Proposals and Voting on algorithmic process for assigning lending and borrowing interest rates.

--- contracts/BMSDao.sol
+++ contracts/BMSDao.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.7;
 
 //import '@openzeppelin/contracts/
 contract BMSDao {
-    //Holders of BMS Token can perticipate in the Dao
-    //Decide the assets for every round
+    //Holders of BMS Token can perticipate in the Dao 
+    //Decide the assets for every round 
     //Decide the moon price for every asset
     //Decide the interest bearing accounts to be used
-    //Decide which charities to be included & which to be depreciaated
+    //Decide which charities to be included & which to be depreciated
+    //Participation in the DAO will follow 2 simple rules to start:
+    //In order to make proposals for the existing game elements open to DAO review, a single or pooled amount of 0.25% of BMS in circulation is necessary.  
+    //All holders can vote on a 1:1 ratio of vote to BMS token. Passing is 2/3 of vote (YES?)
+    // Gov Proposals are voted on for 5 days
+    // Need a requiste number of votes to pass/ register as a process??
+    // 2 day timelock for implementation of proposal if passed. 
+
 }
+
