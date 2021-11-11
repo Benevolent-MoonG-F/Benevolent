1af28dce016fdd930b1565d3694f454aa0b5f9bf
Benevolent Moon Gaming and Finance <91615238+Benevolent-Moon@users.noreply.github.com>
2021-11-11 18:05:40 -0500

    Update BMSDao.sol

--- contracts/BMSDao.sol
+++ contracts/BMSDao.sol
@@ -6,18 +6,18 @@ pragma solidity ^0.8.7;
 //import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 //import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 // import "@openzeppelin/contracts/governance/TimelockController.sol";
-//Above contracts need to be fir to BMGF terminology
+//Above contracts need to be fit to BMGF terminology
 
 //import '@openzeppelin/contracts/
 contract BMSDao {
-    //Holders of BMS Token can perticipate in the Dao 
+    //Holders of BMS Token can participate in the Dao 
     //Decide the assets for every round 
     //Decide the moon price for every asset
     //Decide the interest bearing accounts to be used
     //Decide which charities to be included & which to be depreciated
     //Participation in the DAO will follow 2 simple rules to start:
-    //In order to make proposals for the existing game elements open to DAO review, a single or pooled amount of 0.25% of BMS in circulation is necessary.  
-    //All holders can vote on a 1:1 ratio of vote to BMS token. Passing is 2/3 of vote (YES?)
+    //In order to make proposals for the existing game elements open to DAO review, a single or pooled amount of 0.1% of BMS in circulation is necessary.  
+    //All holders can vote on a 1:1 ratio of vote to BMS token. Passing is 2/3 of vote 
     // Gov Proposals are voted on for 5 days
     // Need a requiste number of votes to pass/ register as a process??
     // 2 day timelock for implementation of proposal if passed. 
