765d3a11190870afceb3b0d4388e376a3ef12ba1
phydy <mphidel@gmail.com>
2021-11-24 23:06:56 +0300

    added interest withdrawal and corrected some previous issues

--- contracts/MoonSquares.sol
+++ contracts/MoonSquares.sol
@@ -21,6 +21,10 @@ import {
 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
 import {
+    IInstantDistributionAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
+
+import {
     IConstantFlowAgreementV1
 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
@@ -45,10 +49,20 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
     IERC20[] public allowedPayments;//list of all accepted stablecoins for placing a prediction
 
     uint128 public coinRound;
+    uint monthCount;
+
+    uint payroundStartTime;
+
+    uint128 constant payDayDuration = 30 days;
+
+    uint totalPaid;
     
 
     address constant IBA = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e; //should be aave contact address or the IBA to be used
     address DAO; //address of the Dao contact
+    address SWAPADRESS;
+    address Dai;
+    address _aaveToken;
     
     mapping (uint256 => uint256) public roundStartPrice;
 
@@ -97,7 +111,8 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
 
     //efficient way to store the arrays
     
-
+    address[] public winers;
+    
     mapping (uint256 => address[]) roundPlayerArray;
 
     mapping (uint256 => uint256[]) roundStartTimeArray;
@@ -131,6 +146,7 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
         _cfa = cfa;
         _acceptedToken = acceptedToken;
         _receiver.push(receiver);
+        payroundStartTime = block.timestamp;
 
 
         uint256 configWord =
@@ -152,12 +168,29 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
         charities.push(_charityName);
         charityAddress.push(_charityAddress);
     }
+    
+    modifier isWinner() {
+        for (uint i =0; i< winers.length; i++) {
+            require(winers[i] == msg.sender);
+        }
+        _;
+    }
 
-    function voteForCharity(bytes8 charity) internal {
+    function voteForCharity(bytes8 charity) public isWinner {
         roundCharityVotes[coinRound][charity] += 1;
     }
     
-   function predictAsset(uint256 _start, uint256 coin /* integer for the index of the stablecoin in allowedPayments*/, uint256 _end) external nonReentrant returns (bytes memory){
+    function addToWinners(address _winner) external {
+        winers.push(_winner);
+    }
+    
+   function predictAsset(
+       uint256 _start, 
+       uint256 coin /* integer for the index of the stablecoin in allowedPayments*/, 
+       uint256 _end,
+       address[] calldata swapPairs
+    ) external nonReentrant returns (bytes memory /*, uint[] memory */)
+    {
         uint256 amount;
         uint256 duration = 300 seconds;
         
@@ -165,10 +198,22 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
             amount = ((getPrice() - roundStartPrice[coinRound]) * 100) + 5 * 10 ** 18;
         } else {
             amount  = 5 * 10 ** 18;
-            
         }
-        
-        bytes memory payload =abi.encodeWithSignature("deposit(address, uint, address, uint)", allowedPayments[coin], amount, address(this), 0);
+
+        //remember to add aprove on the ERC20 to the QuickSwap Rrouter
+        address(SWAPADRESS).call(
+            abi.encodeWithSignature(
+                "swapTokensForExactTokens(uint, uint, address[], address, uint)",
+                amount,//amount out
+                amount,//amount in
+                swapPairs, //pairs geting swaped
+                msg.sender, 
+                1
+            )
+        );
+        //uint[] memory returnValues = address(SWAPADRESS).call(swapload);
+
+        bytes memory payload =abi.encodeWithSignature("deposit(address, uint, address, uint)", Dai, amount, address(this), 0);//should have a protocal referal to use
         (bool success, bytes memory returnData) = address(IBA).call(payload);
         require(success);
         
@@ -183,7 +228,7 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
         allowedPayments[coin].approve(address(this), amount);
         allowedPayments[coin].transferFrom(msg.sender, address(this), amount);
 */
-        roundAssetTotalAmount[coinRound][allowedPayments[coin]] += amount;
+        //roundAssetTotalAmount[coinRound][allowedPayments[coin]] += amount;
         //Dai.transferFrom(msg.sender, address(this), amount);
         //update the betsPlaced mapping
         roundAddressBetsPlaced[coinRound][msg.sender].squareStartTime = _start;
@@ -197,11 +242,13 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
         roundEndTimeArray[coinRound].push(_end);
         //update the total value played
         roundTotalStaked[coinRound] += amount;
-        roundCharityContributions[coinRound] = (roundTotalStaked[coinRound] * 7)/100;
+        //roundCharityContributions[coinRound] = (roundTotalStaked[coinRound] * 7)/100;
         roundWinnings[coinRound] = (roundTotalStaked[coinRound] * 90)/100;
-        roundContractContribution[coinRound] = (roundTotalStaked[coinRound] * 3)/100;
+        //roundContractContribution[coinRound] = (roundTotalStaked[coinRound] * 3)/100;
         
-        return returnData;
+        //this.voteForCharity(_charity);
+        
+        return (returnData /*, returnValues */);
     }
 
     function getPrice() internal view returns(uint256){
@@ -239,6 +286,7 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
     }
 
     //should use superflid's flow if its just one user & instant distribution if there are several winners
+
      function givePrize(uint256 _round, bytes calldata ctx) internal returns (bytes memory newCtx) {
         require(roundWinningIndex[_round].length != 0);
         int96 inFlowRate;
@@ -250,7 +298,7 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
                 abi.encodeWithSelector(
                     _cfa.createFlow.selector,
                     _acceptedToken,
-                    roundWinningIndex[0],
+                    roundWinningIndex[0], //use addresses
                     inFlowRate,
                     new bytes(0) // placeholder
                 ),
@@ -263,7 +311,17 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
                 //Instant Distribution Logic
             }
         }
+        totalPaid += roundWinnings[coinRound];
 
+    }
+
+    function setTime(uint128 _round) private {
+        roundWinningTime[_round] = getTime();
+        setwinningIndex();
+    }
+
+    function getRound() public view returns(uint128) {
+        return coinRound;
     }
 
     function checkUpkeep(
@@ -273,13 +331,22 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
     ) {
         if (getPrice() == roundMoonPrice[coinRound]) {
             upkeepNeeded = true;
-            return (true, performData) ;
+            return (true, /* address(this).call( */ abi.encodeWithSignature("setTime(uint128)", getRound()));
         }
+        if (roundWinningIndex[coinRound].length != 0) {
+            upkeepNeeded = true;
+            return (true, abi.encodeWithSignature("withdrawRoundFundsFromIba(uint256)", getRound()));//
+        }
+        if (block.timestamp >= payroundStartTime + 30 days) {
+            upkeepNeeded = true;
+            return (true, abi.encodeWithSignature("withdrawInterest(address)", _aaveToken));
+        }
+        performData = checkData;
+        
     }
 
-    function performUpkeep(bytes calldata /* performData */) external override {
-        roundWinningTime[coinRound] = getTime();
-        setwinningIndex();
+    function performUpkeep(bytes calldata performData) external override {
+        performData;
     }
 
     function tokenIsAccepted(IERC20 _token) public view returns (bool) {
@@ -304,11 +371,9 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
     }
 
     //withdraws the total Amount after the moonpice gets hit
-    function withdrawRoundFundsFromIba(/*uint256 _round*/) private returns (bytes memory) {
+    function withdrawRoundFundsFromIba(uint128 _round) private returns (bytes memory) {
         //if (getPrice() == roundMoonPrice[_round]) {
-        for (uint i = 0; i < allowedPayments.length; i++) {
-                
-            bytes memory payload =abi.encodeWithSignature("withdraw(address, uint, address)", allowedPayments[i], ((roundAssetTotalAmount[coinRound][allowedPayments[i]] * 90)/ 100), address(this));
+            bytes memory payload =abi.encodeWithSignature("withdraw(address, uint, address)", Dai, roundWinnings[_round], address(this));
             (bool success, bytes memory returnData) = address(IBA).call(payload);
             require(success);
                 
@@ -318,212 +383,44 @@ contract MoonSquares is SuperAppBase, KeeperCompatibleInterface, Ownable, Reentr
                 address(this)
             ); */
             return returnData;
-        }
         //}
         //Withdraws Funds from the predictions and the interest earned
     }
 
-    //sends the rounds's charity donations to the winning charity
-    function sendToWinningCharity(uint256 _round) internal {
-        //Flow
+    function withdrawInterest(address aaveToken) private returns(uint, bytes memory) {
+        uint aaveBalance = IERC20(aaveToken).balanceOf(msg.sender);
+        uint interest = aaveBalance - (roundTotalStaked[coinRound] - totalPaid);
+        bytes memory payload = abi.encodeWithSignature(
+            "withdraw(address, uint, address)",
+            Dai,
+            interest,
+            address(this)
+        );
+        (bool success, bytes memory returnData) = address(IBA).call(payload);
+        require(success);
+        return(interest, returnData);
+
     }
+
+    //sends the rounds's charity donations to the winning charity
 
     //distributes the Interest Earned on the round to members of the Dao
-    function flowToDao(uint256 _round) internal {
-        //Flow
-    }
-
-    function flowToDevs(uint256 _round) internal {
-        //Instant Distribution
-    }
-    
-    function currentReceiver()
-        external view
-        returns (
-            uint256 startTime,
-            address[]memory receiver,
-            int96 flowRate
-        )
-    {
-        for (uint i =0; i < _receiver.length; i++) {
-            if (_receiver[i] != address(0)) {
-                (startTime, flowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[i]);
-                receiver = _receiver;
-            }
-            
-        }
-    }
-
-    event ReceiverChanged(address receiver); //what is this?
-
-    /// @dev If a new stream is opened, or an existing one is opened
-    function _updateOutflow(bytes calldata ctx)
-        private
-        returns (bytes memory newCtx)
-    {
-      newCtx = ctx;
-      // @dev This will give me the new flowRate, as it is called in after callbacks
-      int96 netFlowRate = _cfa.getNetFlow(_acceptedToken, address(this));
-      (,int96 outFlowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[1]); // CHECK: unclear what happens if flow doesn't exist.
-      int96 inFlowRate = netFlowRate + outFlowRate;
-
-      // @dev If inFlowRate === 0, then delete existing flow.
-      if (inFlowRate == int96(0)) {
-        // @dev if inFlowRate is zero, delete outflow.
-          (newCtx, ) = _host.callAgreementWithContext(
-              _cfa,
-              abi.encodeWithSelector(
-                  _cfa.deleteFlow.selector,
-                  _acceptedToken,
-                  address(this),
-                  _receiver,
-                  new bytes(0) // placeholder
-              ),
-              "0x",
-              newCtx
-          );
-        } else if (outFlowRate != int96(0)){
+    function flowToDao(/*uint256 _round,*/ bytes calldata ctx) external returns(bytes memory newCtx){
+        //Flows interest earned from the protocal to the redirectAll contract that handles distribution
+        int96 inFlowRate;
+        newCtx = ctx;
+            //Flow Winnings
         (newCtx, ) = _host.callAgreementWithContext(
             _cfa,
             abi.encodeWithSelector(
-                _cfa.updateFlow.selector,
+                _cfa.createFlow.selector,
                 _acceptedToken,
-                _receiver,
-                inFlowRate,
+                DAO,
+                inFlowRate, //should be the total amount of Interest withdrawnfrom the IBA divided by the number of seconds in the withdrawal interval
                 new bytes(0) // placeholder
             ),
             "0x",
             newCtx
         );
-      } else {
-      // @dev If there is no existing outflow, then create new flow to equal inflow
-          (newCtx, ) = _host.callAgreementWithContext(
-              _cfa,
-              abi.encodeWithSelector(
-                  _cfa.createFlow.selector,
-                  _acceptedToken,
-                  _receiver,
-                  inFlowRate,
-                  new bytes(0) // placeholder
-              ),
-              "0x",
-              newCtx
-          );
-      }
     }
-
-    // @dev Change the Receiver of the total flow
-    function _changeReceiver( address newReceiver ) internal {
-        require(newReceiver != address(0));
-        // @dev because our app is registered as final, we can't take downstream apps
-        require(!_host.isApp(ISuperApp(newReceiver)));
-        if (newReceiver == _receiver[3]) return ;
-        // @dev delete flow to old receiver
-        (,int96 outFlowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[3]); //CHECK: unclear what happens if flow doesn't exist.
-        if(outFlowRate > 0){
-          _host.callAgreement(
-              _cfa,
-              abi.encodeWithSelector(
-                  _cfa.deleteFlow.selector,
-                  _acceptedToken,
-                  address(this),
-                  _receiver,
-                  new bytes(0)
-              ),
-              "0x"
-          );
-          // @dev create flow to new receiver
-          _host.callAgreement(
-              _cfa,
-              abi.encodeWithSelector(
-                  _cfa.createFlow.selector,
-                  _acceptedToken,
-                  newReceiver,
-                  _cfa.getNetFlow(_acceptedToken, address(this)),
-                  new bytes(0)
-              ),
-              "0x"
-          );
-        }
-        // @dev set global receiver to new receiver
-        _receiver[3] = newReceiver;
-
-        emit ReceiverChanged(_receiver[3]);
-    }
-
-    /**************************************************************************
-     * SuperApp callbacks
-     *************************************************************************/
-
-
-    function afterAgreementCreated(
-        ISuperToken _superToken,
-        address _agreementClass,
-        bytes32, // _agreementId,
-        bytes calldata /*_agreementData*/,
-        bytes calldata ,// _cbdata,
-        bytes calldata _ctx
-    )
-        external override
-        onlyExpected(_superToken, _agreementClass)
-        onlyHost
-        returns (bytes memory newCtx)
-    {
-        return _updateOutflow(_ctx);
-    }
-
-    function afterAgreementUpdated(
-        ISuperToken _superToken,
-        address _agreementClass,
-        bytes32 ,//_agreementId,
-        bytes calldata agreementData,
-        bytes calldata ,//_cbdata,
-        bytes calldata _ctx
-    )
-        external override
-        onlyExpected(_superToken, _agreementClass)
-        onlyHost
-        returns (bytes memory newCtx)
-    {
-        return _updateOutflow(_ctx);
-    }
-
-    function afterAgreementTerminated(
-        ISuperToken _superToken,
-        address _agreementClass,
-        bytes32 ,//_agreementId,
-        bytes calldata /*_agreementData*/,
-        bytes calldata ,//_cbdata,
-        bytes calldata _ctx
-    )
-        external override
-        onlyHost
-        returns (bytes memory newCtx)
-    {
-        // According to the app basic law, we should never revert in a termination callback
-        if (!_isSameToken(_superToken) || !_isCFAv1(_agreementClass)) return _ctx;
-        return _updateOutflow(_ctx);
-    }
-
-    function _isSameToken(ISuperToken superToken) private view returns (bool) {
-        return address(superToken) == address(_acceptedToken);
-    }
-
-    function _isCFAv1(address agreementClass) private view returns (bool) {
-        return ISuperAgreement(agreementClass).agreementType()
-            == keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
-    }
-
-    modifier onlyHost() {
-        require(msg.sender == address(_host), "RedirectAll: support only one host");
-        _;
-    }
-
-    modifier onlyExpected(ISuperToken superToken, address agreementClass) {
-        require(_isSameToken(superToken), "RedirectAll: not accepted token");
-        require(_isCFAv1(agreementClass), "RedirectAll: only CFAv1 supported");
-        _;
-    }
-    
-
 }
---
+++ contracts/Redirect.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import {
+    ISuperfluid,
+    ISuperToken,
+    ISuperApp,
+    ISuperAgreement,
+    SuperAppDefinitions
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import {
+    IConstantFlowAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import {
+    SuperAppBase
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+
+contract RedirectAll is SuperAppBase {
+
+    ISuperfluid private _host; // host
+    IConstantFlowAgreementV1 private _cfa; // the stored constant flow agreement class address
+    ISuperToken private _acceptedToken; // accepted token
+    address[] public _receiver;
+    address MoonSquareAddress;
+
+    constructor(
+        ISuperfluid host,
+        IConstantFlowAgreementV1 cfa,
+        ISuperToken acceptedToken,
+        address dev,
+        address dao,
+        address moon) {
+        require(address(host) != address(0), "host is zero address");
+        require(address(cfa) != address(0), "cfa is zero address");
+        require(address(acceptedToken) != address(0), "acceptedToken is zero address");
+        require(address(dev) != address(0), "receiver is zero address");
+        require(!host.isApp(ISuperApp(dev)), "receiver is an app");
+
+        _host = host;
+        _cfa = cfa;
+        _acceptedToken = acceptedToken;
+        MoonSquareAddress = moon;
+        _receiver.push(dev);
+        _receiver.push(dao);
+
+        uint256 configWord =
+            SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+
+        _host.registerApp(configWord);
+    }
+
+
+    /**************************************************************************
+     * Redirect Logic
+     *************************************************************************/
+
+    function currentReceiver()
+        external view
+        returns (
+            uint256 startTime,
+            address[] memory receiver,
+            int96 flowRate
+        )
+    {
+        for (uint i = 0; i < _receiver.length; i++){
+            if (_receiver[i] != address(0)) {
+                (startTime, flowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[i]);
+                receiver = _receiver;
+            }
+        }
+
+    }
+
+    event ReceiverChanged(address receiver); //what is this?
+
+    /// @dev If a new stream is opened, or an existing one is opened
+    function _updateOutflow(bytes calldata ctx)
+        private
+        returns (bytes memory newCtx)
+    {
+      newCtx = ctx;
+      // @dev This will give me the new flowRate, as it is called in after callbacks
+      for (uint96 i = 0; i < _receiver.length; i++){
+        int96 netFlowRate = _cfa.getNetFlow(_acceptedToken, address(this));
+        (,int96 outFlowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[0]); // CHECK: unclear what happens if flow doesn't exist.
+        int96 inFlowRate = netFlowRate + (outFlowRate * 3);
+    
+          // @dev If inFlowRate === 0, then delete existing flow.
+          if (inFlowRate == int96(0)) {
+            // @dev if inFlowRate is zero, delete outflow.
+              (newCtx, ) = _host.callAgreementWithContext(
+                  _cfa,
+                  abi.encodeWithSelector(
+                      _cfa.deleteFlow.selector,
+                      _acceptedToken,
+                      address(this),
+                      _receiver[i],
+                      new bytes(0) // placeholder
+                  ),
+                  "0x",
+                  newCtx
+              );
+            } else if (outFlowRate != int96(0)){
+            (newCtx, ) = _host.callAgreementWithContext(
+                _cfa,
+                abi.encodeWithSelector(
+                    _cfa.updateFlow.selector,
+                    _acceptedToken,
+                    _receiver[i],
+                    (inFlowRate/3),
+                    new bytes(0) // placeholder
+                ),
+                "0x",
+                newCtx
+            );
+          } else {
+          // @dev If there is no existing outflow, then create new flow to equal inflow
+              (newCtx, ) = _host.callAgreementWithContext(
+                  _cfa,
+                  abi.encodeWithSelector(
+                      _cfa.createFlow.selector,
+                      _acceptedToken,
+                      _receiver[i],
+                      (inFlowRate/3),
+                      new bytes(0) // placeholder
+                  ),
+                  "0x",
+                  newCtx
+              );
+          }   
+      }
+    }
+
+    modifier onlyMoonSquares() {
+        require(msg.sender == MoonSquareAddress);
+        _;
+    }
+
+    function changeReceiverAdress(address _newReceiver) external {
+       _changeReceiver(_newReceiver);
+    }
+
+    // @dev Change the Receiver of the total flow
+    function _changeReceiver( address newReceiver ) internal {
+        require(newReceiver != address(0), "New receiver is zero address");
+        // @dev because our app is registered as final, we can't take downstream apps
+        require(!_host.isApp(ISuperApp(newReceiver)), "New receiver can not be a superApp");
+        for (uint i=0; i< _receiver.length; i++) {
+            require(newReceiver != _receiver[i]);
+            if (newReceiver == _receiver[i]) return ;
+    
+        
+        // @dev delete flow to old receiver
+        
+            (,int96 outFlowRate,,) = _cfa.getFlow(_acceptedToken, address(this), _receiver[0]); //CHECK: unclear what happens if flow doesn't exist.
+            if(outFlowRate > 0){
+              _host.callAgreement(
+                  _cfa,
+                  abi.encodeWithSelector(
+                      _cfa.deleteFlow.selector,
+                      _acceptedToken,
+                      address(this),
+                      _receiver[2],
+                      new bytes(0)
+                  ),
+                  "0x"
+              );
+              // @dev create flow to new receiver
+              _host.callAgreement(
+                  _cfa,
+                  abi.encodeWithSelector(
+                      _cfa.createFlow.selector,
+                      _acceptedToken,
+                      newReceiver,
+                      (_cfa.getNetFlow(_acceptedToken, address(this))/3),
+                      new bytes(0)
+                  ),
+                  "0x"
+              );
+            }
+            // @dev set global receiver to new receiver
+            _receiver[3] = newReceiver;
+    
+            emit ReceiverChanged(_receiver[2]);
+        }
+    }
+
+    /**************************************************************************
+     * SuperApp callbacks
+     *************************************************************************/
+
+    function afterAgreementCreated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata /*_agreementData*/,
+        bytes calldata ,// _cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+        return _updateOutflow(_ctx);
+    }
+
+    function afterAgreementUpdated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32 ,//_agreementId,
+        bytes calldata /*agreementData */,
+        bytes calldata ,//_cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+        return _updateOutflow(_ctx);
+    }
+
+    function afterAgreementTerminated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32 ,//_agreementId,
+        bytes calldata /*_agreementData*/,
+        bytes calldata ,//_cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+        // According to the app basic law, we should never revert in a termination callback
+        if (!_isSameToken(_superToken) || !_isCFAv1(_agreementClass)) return _ctx;
+        return _updateOutflow(_ctx);
+    }
+
+    function _isSameToken(ISuperToken superToken) private view returns (bool) {
+        return address(superToken) == address(_acceptedToken);
+    }
+
+    function _isCFAv1(address agreementClass) private view returns (bool) {
+        return ISuperAgreement(agreementClass).agreementType()
+            == keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
+    }
+
+    modifier onlyHost() {
+        require(msg.sender == address(_host), "RedirectAll: support only one host");
+        _;
+    }
+
+    modifier onlyExpected(ISuperToken superToken, address agreementClass) {
+        require(_isSameToken(superToken), "RedirectAll: not accepted token");
+        require(_isCFAv1(agreementClass), "RedirectAll: only CFAv1 supported");
+        _;
+    }
+
+}
